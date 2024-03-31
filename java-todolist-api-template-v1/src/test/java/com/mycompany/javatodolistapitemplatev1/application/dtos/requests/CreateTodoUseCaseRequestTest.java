70ad9d9bc9e0f427be1dc699a3cfcc2c1e344149
Leandro Andrade <eusouleoandrade@gmail.com>
2024-03-31 17:12:27 -0300

    Adiciona o teste unitário CreateTodoUseCaseRequestTest.

---
+++ java-todolist-api-template-v1/src/test/java/com/mycompany/javatodolistapitemplatev1/application/dtos/requests/CreateTodoUseCaseRequestTest.java
@@ -0,0 +1,8 @@
+package com.mycompany.javatodolistapitemplatev1.application.dtos.requests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreateTodoUseCaseRequestTest {
+
+}
