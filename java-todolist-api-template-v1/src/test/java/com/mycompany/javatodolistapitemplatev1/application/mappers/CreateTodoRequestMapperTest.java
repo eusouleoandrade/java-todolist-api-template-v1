18252a809c846c4ddc4d9157d610234956ba2133
Leandro Andrade <eusouleoandrade@gmail.com>
2024-04-12 08:48:44 -0300

    Cria testes para os mappers.

---
+++ java-todolist-api-template-v1/src/test/java/com/mycompany/javatodolistapitemplatev1/application/mappers/CreateTodoRequestMapperTest.java
@@ -0,0 +1,8 @@
+package com.mycompany.javatodolistapitemplatev1.application.mappers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreateTodoRequestMapperTest {
+
+}
